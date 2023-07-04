46588f19273216854c433b576ef382cbd19564d7
preethamRajalabandi <preethamrajalabandi007@gmail.com>
2023-07-04 15:09:34 +0530

    Video 44 - Routes and Schemas

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const connectoMongo = require('./db')
 
 connectoMongo();
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Harry!')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
