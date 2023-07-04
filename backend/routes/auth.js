6f3e03c8fc1a045dad0792bf50ac96a9d103d56b
preethamRajalabandi <preethamrajalabandi007@gmail.com>
2023-07-04 21:16:57 +0530

    video 48 - bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 
-
+const JWT_SEC = 'Preethamisagoodb$oy'
 
 // Create a User using: POST "api/auth/createuser". No login required
 router.post('/createuser',
@@ -24,12 +26,21 @@ router.post('/createuser',
             if (user) {
                 return res.status(400).json({ errors: "Sorry a user with this email already exists" });
             }
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
             user = await User.create({
-                name: req.body.name,
+                name: req.body.name, 
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass
             })
-            res.json(user)
+            const data = {
+                user : {
+                    is: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SEC);
+            res.json({authToken})
         }
         catch (error) {
             console.error(error.message);
