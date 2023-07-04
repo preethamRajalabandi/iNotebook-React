36960fe856741eb0cd9f6254ea6ad3d60a1435ee
preethamRajalabandi <preethamrajalabandi007@gmail.com>
2023-07-04 10:46:12 +0530

    Video 43 - db and express

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+
+const connectoMongo = () => {
+    mongoose.connect(mongoURI).then(
+        (data) => {
+            console.log(`Connected to Mongo Successfully `)
+        }
+    ).catch(
+        (err) => {
+            console.log(err)
+
+        }
+    )
+}
+
+module.exports = connectoMongo
