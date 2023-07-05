808427d987b1462ace85c0697c604b29d34c5336
preethamRajalabandi <preethamrajalabandi007@gmail.com>
2023-07-05 08:20:34 +0530

    video 53 - updating existing note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -16,7 +16,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 })
 
 // Route 2 : Add a new route: POST "/api/notes/addnote". Login required
-router.get('/addnote', fetchuser, [
+router.post('/addnote', fetchuser, [
     body('title', "Enter a valid natitleme").isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
@@ -39,4 +39,25 @@ router.get('/addnote', fetchuser, [
     }
 })
 
+// Route 3 : Update an existing Note using: POST "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body
+
+    // Create a newNote object
+    const newNote  = {};
+    if(title){newNote.title = title}
+    if(description){newNote.description=description}
+    if(tag){newNote.tag=tag} 
+
+    //Find the note to be updated and updates it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    if(note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed")
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+})
 module.exports = router
